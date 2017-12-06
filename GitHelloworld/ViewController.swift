2bf9556c21a31bf9be100a9223da3eb0e8db636b
Periyar Meadows <periyarmeadows@Periyars-Mac-mini.local>
2017-12-06 16:00:20 +0530

    second commit

--- GitHelloworld/ViewController.swift
+++ GitHelloworld/ViewController.swift
@@ -14,8 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello World")
+        print("This Branch Second")
     }
 
+    @IBAction func click(_ sender: UIButton) {
+        
+        if sender.tag == 0 {
+            sender.tag = 1
+            self.view.backgroundColor = UIColor.green
+        } else {
+            sender.tag = 0
+            self.view.backgroundColor = UIColor.purple
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
